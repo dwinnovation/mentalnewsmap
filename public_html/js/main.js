e5336f0e260b650a3f7556c43d244b2e3ecc46a8
hannesrohde <hannes.rohde@web.de>
2014-09-03 11:01:23 +0200

    start tab

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,6 +4,35 @@ $('#tabnav a').click(function (e) {
   $(this).tab('show')
 })
 
+var state {};
+
+/**
+ * reset the game state:
+ */
+function initState() {
+    state = {
+        points: 0,
+        remainingQuestions: [],
+        answeredQuestions: [],
+    };
+}
+
+/**
+ * init the "start" jumbotron
+ */
+function initJumboStart() {
+    $('#jumbostart .category a').click(function(){
+        var category = $(this).data('category');
+        if (typeof category == 'undefined') {
+            // all categories
+        } else {
+            // selected category
+        }
+    });
+}
+
 $( document ).ready(function(){
     // add code to run after page load here
+    initJumboStart();
+    initState();
 });
