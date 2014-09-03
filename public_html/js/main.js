5d36ae794d83e9a7b2482a8242db205e7ed702c7
hannesrohde <hannes.rohde@web.de>
2014-09-03 11:31:04 +0200

    game workflow

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -12,27 +12,65 @@ var state = {};
 function initState() {
     state = {
         points: 0,
-        remainingQuestions: [],
-        answeredQuestions: [],
+        remainingArticles: [],
+        answeredArticles: [],
     };
+}
+
+function showArticle() {
+    // activate "game" tab:
+    $('#tabnav a[href="#game"]').tab('show');
+    // load question to view:
+    // TODO
+}
+
+/**
+ * Main game workflow function
+ * @param state
+ */
+function switchGameState(state) {
+    switch (state) {
+    case 'start':
+        initState();
+        break;
+    case 'game':
+        showArticle();
+        break;
+    case 'result':
+        // TODO
+        break;
+    case 'finish':
+        // TODO
+        break;
+    }
 }
 
 /**
  * init the "start" jumbotron
  */
 function initJumboStart() {
-    $('#jumbostart .category a').click(function(){
+    // handle clicks on category selection links:
+    $('#start .category a').click(function(){
         var category = $(this).data('category');
+        // load article (sub-)set into state variable:
         if (typeof category == 'undefined') {
-            // all categories
+            // all categories -> all articles
+            state.remainingArticles = articles;
         } else {
-            // selected category
+            // selected category -> filter articles
+            state.remainingArticles = $.grep(articles, function(art){
+                return art.kategorie == category}
+            );
         }
+        // start the gane by switching to state "game"
+        switchGameState('game');
     });
 }
 
 $( document ).ready(function(){
     // add code to run after page load here
     initJumboStart();
-    initState();
+    
+    // start the game:
+    switchGameState('start');
 });
