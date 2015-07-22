1d16a686f4c971e830738993d79c9165d87dbba5
Hannes Rohde <hannes.rohde@web.de>
2015-07-22 13:06:04 +0200

    "fixed" license statement

--- public_html/js/mapFunctions.js
+++ public_html/js/mapFunctions.js
@@ -1,18 +1,7 @@
-/* 
- * Copyright 2014 Milan Burgmann.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * This script file contains various code related to the world map.
  */
+
 var map; //The map Obj.
 var clickPosition; //Holds the current lat, lng for the clicked point on the map
 var p1; //Position the player clicked
